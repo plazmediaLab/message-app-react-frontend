9eb4a479e9007da7428a8f3709f507da4c953682
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-05-12 18:53:55 -0500

    Primera configuración base del proyecto

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Helmet } from 'react-helmet';
+// Modular CSS
+import "modular-css/css/modular.css";
+import "modular-css/css/modular-css.font.css";
+import { ModularCSSfonstRequired } from 'modular-css';
+// Favicon
+import Favicon from './img/isologo.png';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <title>GraphQL Message App</title>
+      <link rel="icon" href={Favicon} />
+      <meta name="theme-color" content="#191919" />
+      <link href={ModularCSSfonstRequired} rel="stylesheet" />
+    </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
